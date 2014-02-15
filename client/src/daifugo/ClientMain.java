2982a3b60d9428d5b822e45501f6b14d77eeba78
worldbright <sushionigiris@gmail.com>
2014-02-15 22:48:44 +0900

    Refactor ClientMain

--- client/src/daifugo/ClientMain.java
+++ client/src/daifugo/ClientMain.java
@@ -10,27 +10,35 @@ import javax.swing.*;
 /**
  * This class is responsible for displaying the login window. Once the player
  * chooses a room and enters his or her username and password, this class tells
- * the DaifugoClient to create a new connection using the provided information.
- * This class is the entry point for clients.
+ * the DaifugoClient to create a new connection using the provided
+ * information.
  */
 public class ClientMain {
 
     /**
-     * Modify this IP address in order to connect to proper host. If you plan to
-     * test the application from you own computer, use localhost.
+     * Modify this IP address in order to readPortAndConnect to proper host. If
+     * you plan to test the application from you own computer, use localhost.
      */
     private static final String HOST = "localhost"; //162.243.121.40
+    
+    private static final int TEST = 23548;
+    private static final int BLITZ = 23549;
+    private static final int TOURNAMENT = 23550;
+    private static final int ACCOUNT = 23551;
 
-    private DaifugoClient client;
+    private enum Mode {
+        PLAY, ACCOUNT;
+    }
+
     private int port;
-    private Dimension screenSize = Toolkit.getDefaultToolkit().getScreenSize();
+    private final Dimension screenSize = Toolkit.getDefaultToolkit().getScreenSize();
     private final int WIDTH = 500;
     private final int HEIGHT = 300;
     private JFrame frame;
     private JPanel content;
     private JPanel topPanel;
     private JLabel titleMessage;
-    final String[] roomChoices = {"Test", "Blitz", "Tournament"};
+    private final String[] roomChoices = {"Test", "Blitz", "Tournament"};
     private JPanel roomsRow;
     private JComboBox rooms;
     private JLabel roomsLabel;
@@ -44,58 +52,13 @@ public class ClientMain {
     private JButton loginButton;
     private JButton cancelButton;
     private JButton createNew;
-
+    
     ClientMain() {
-        topPanel();
-
-        this.loginButton = new JButton("Login");
-        this.cancelButton = new JButton("Cancel");
-        this.createNew = new JButton("Create Account");
-
-        this.bottomPanel = new JPanel();
-        this.bottomPanel.add(this.loginButton);
-        this.bottomPanel.add(this.cancelButton);
-        this.bottomPanel.add(this.createNew);
-
-        this.loginButton.addActionListener(new ActionListener() {
-            public void actionPerformed(ActionEvent ae) {
-                try {
-                    connect();
-                    frame.setVisible(false);
-                } catch (Exception ex) {
-                    Logger.getLogger(ClientMain.class.getName()).log(Level.SEVERE, null, ex);
-                }
-            }
-        });
-        this.cancelButton.addActionListener(new ActionListener() {
-
-            public void actionPerformed(ActionEvent ae) {
-                System.exit(0);
-            }
-        });
-        this.createNew.addActionListener(new ActionListener() {
-            public void actionPerformed(ActionEvent ae) {
-                try {
-                    createAccount();
-                    frame.setVisible(false);
-                } catch (Exception e) {
-                    Logger.getLogger(ClientMain.class.getName()).log(Level.SEVERE, null, e);
-                }
-            }
-        });
-
-        this.content = new JPanel();
-        this.content.add(this.topPanel);
-        this.content.add(this.bottomPanel);
-
-        this.frame = new JFrame("Daifugo");
-        this.frame.setContentPane(this.content);
-        this.frame.setLocation((this.screenSize.width - 500) / 2, (this.screenSize.height - 300) / 2);
-
-        this.frame.setDefaultCloseOperation(3);
-        this.frame.setVisible(true);
-        this.frame.setResizable(false);
-        this.frame.setSize(500, 300);
+        configureTopPanel();
+        configureBottomPanel();
+        configureButtonListeners();
+        configureOverallPanel();
+        configureFrame();
     }
 
     ClientMain(String title) {
@@ -103,57 +66,11 @@ public class ClientMain {
         this.titleMessage.setText(title);
     }
 
-    @SuppressWarnings("ResultOfObjectAllocationIgnored")
     public static void main(String[] args) {
         new ClientMain();
     }
 
-    private String readUsername() {
-        return this.username.getText();
-    }
-
-    private String readPassword() {
-        return String.valueOf(this.password.getPassword());
-    }
-
-    private void connect() throws Exception {
-        this.port = readPort();
-        if (this.port == -1) {
-            throw new Exception("Unsupported Room");
-        }
-        try {
-            this.client = new DaifugoClient(HOST, this.port, readUsername(), readPassword());
-        } catch (IOException ex) {
-            JOptionPane.showMessageDialog(JOptionPane.getRootFrame(), "Application cannot connect to the room. The room is most likely full. If you cannot connect to any room, there might be a problem with your network settings.");
-            System.exit(1);
-        }
-    }
-
-    private void createAccount() throws Exception {
-        this.port = 23551;
-        try {
-            this.client = new DaifugoClient(HOST, this.port, readUsername(), readPassword());
-        } catch (IOException e) {
-            JOptionPane.showMessageDialog(JOptionPane.getRootFrame(), "Application failed to create new account.");
-            System.exit(1);
-        }
-    }
-
-    private int readPort() {
-        String room = (String) this.rooms.getSelectedItem();
-        if (room.equalsIgnoreCase("Test")) {
-            return 23548;
-        }
-        if (room.equalsIgnoreCase("Blitz")) {
-            return 23549;
-        }
-        if (room.equalsIgnoreCase("Tournament")) {
-            return 23550;
-        }
-        return -1;
-    }
-
-    private void topPanel() {
+    private void configureTopPanel() {
         this.titleMessage = new JLabel("Welcome to Daifugo!!!", 0);
         this.titleMessage.setFont(new Font("Baskerville", 0, 30));
         this.titleMessage.setPreferredSize(new Dimension(300, 80));
@@ -183,4 +100,111 @@ public class ClientMain {
         this.topPanel.add(this.usernameRow);
         this.topPanel.add(this.passwordRow);
     }
+
+    private void configureBottomPanel() {
+        this.loginButton = new JButton("Login");
+        this.cancelButton = new JButton("Cancel");
+        this.createNew = new JButton("Create Account");
+
+        this.bottomPanel = new JPanel();
+        this.bottomPanel.add(this.loginButton);
+        this.bottomPanel.add(this.cancelButton);
+        this.bottomPanel.add(this.createNew);
+    }
+
+    private void configureButtonListeners() {
+        this.loginButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent ae) {
+                try {
+                    readPortAndConnect();
+                    frame.setVisible(false);
+                } catch (Exception ex) {
+                    Logger.getLogger(ClientMain.class.getName()).log(Level.SEVERE, null, ex);
+                }
+            }
+        });
+        this.cancelButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent ae) {
+                System.exit(0);
+            }
+        });
+        this.createNew.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent ae) {
+                try {
+                    createAccount();
+                    frame.setVisible(false);
+                } catch (Exception e) {
+                    Logger.getLogger(ClientMain.class.getName()).log(Level.SEVERE, null, e);
+                }
+            }
+        });
+    }
+
+    private void configureOverallPanel() {
+        this.content = new JPanel();
+        this.content.add(this.topPanel);
+        this.content.add(this.bottomPanel);
+    }
+
+    private void configureFrame() throws HeadlessException {
+        this.frame = new JFrame("Daifugo");
+        this.frame.setContentPane(this.content);
+        this.frame.setLocation((this.screenSize.width - WIDTH) / 2, (this.screenSize.height - HEIGHT) / 2);
+        this.frame.setVisible(true);
+        this.frame.setResizable(false);
+        this.frame.setSize(WIDTH, HEIGHT);
+    }
+
+    private void readPortAndConnect() throws Exception {
+        this.port = readPort();
+        connect(Mode.PLAY);
+    }
+
+    private int readPort() {
+        String room = (String) this.rooms.getSelectedItem();
+        switch (room) {
+            case "Test":
+                return TEST;
+            case "Blitz":
+                return BLITZ;
+            case "default":
+                return TOURNAMENT;
+        }
+        return 0;
+    }
+
+    private void createAccount() throws Exception {
+        this.port = ACCOUNT;
+        connect(Mode.ACCOUNT);
+    }
+
+    private void connect(Mode method) throws HeadlessException {
+        try {
+            new DaifugoClient(HOST, this.port, readUsername(), readPassword());
+        } catch (IOException ex) {
+            displayErrorToUser(method);
+            ex.printStackTrace();
+            System.exit(1);
+        }
+    }
+
+    private String readUsername() {
+        return this.username.getText();
+    }
+
+    private String readPassword() {
+        return String.valueOf(this.password.getPassword());
+    }
+
+    private void displayErrorToUser(Mode method) throws HeadlessException {
+        if (method == Mode.PLAY) {
+            JOptionPane.showMessageDialog(JOptionPane.getRootFrame(), "Application cannot connect to the room. The room is most likely full. If you cannot connect to any room, there might be a problem with your network settings.");
+        } else {
+            JOptionPane.showMessageDialog(JOptionPane.getRootFrame(), "Application failed to create new account.");
+        }
+    }
+
 }
