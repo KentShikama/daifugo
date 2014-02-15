bf4d16f8e9a526253b3d29882419e88dbd16aa1f
worldbright <sushionigiris@gmail.com>
2014-02-15 21:53:54 +0900

    Refactor HubMain

--- server/src/daifugo/HubMain.java
+++ server/src/daifugo/HubMain.java
@@ -6,28 +6,35 @@ import java.util.Scanner;
 
 public class HubMain {
 
-    private final static int[] PORT = {23548, 23549, 23550, 23551};
-    ArrayList<DaifugoHub> esoterics = new ArrayList<>();
+    private final static int[] PORTS = {23548, 23549, 23550, 23551};
+    private ArrayList<DaifugoHub> esoterics = new ArrayList<>();
 
     HubMain() {
+        createHubs();
+        detectQuit();
+    }
+
+    private void createHubs() {
         try {
-            for (int i = 0; i < PORT.length; i++) {
-                esoterics.add(new DaifugoHub(PORT[i]));
+            for (int i = 0; i < PORTS.length; i++) {
+                esoterics.add(new DaifugoHub(PORTS[i]));
             }
         } catch (IOException e) {
-            System.out.println("Can't create listening socket.  Shutting down.");
+            System.out.println("Can't create listening socket. Shutting down.");
         }
+    }
+
+    private void detectQuit() {
         try (Scanner scanner = new Scanner(System.in)) {
             while (scanner.hasNext()) {
                 String quit = scanner.next();
-                if (quit.equals("9")) {
+                if (quit.equals("q")) {
                     System.exit(0);
                 }
             }
         } catch (Exception e) {
             e.printStackTrace();
         }
-
     }
 
     public static void main(String[] args) {
